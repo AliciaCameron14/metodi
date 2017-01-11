f2836ea5d04040649bd7d0dd3fbd21d3e9047096
AliciaCameron14 <cameronalicia@gmail.com>
2017-01-11 10:46:47 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+  <title>MeToDi</title>
+
+<!-- To Do: Batch load -->
+  <!-- Include JS Scripts  -->
+  <script src="./js/000-lib/000-jquery-2.2.1.min.js"></script>
+  <script src="./js/000-lib/001-jquery-ui-1.12.1.min.js"></script>
+  <script src="./js/000-lib/002-bootstrap.min.js"></script>
+  <script src="./js/000-lib/003-angular.min.js"></script>
+  <script src="./js/000-lib/004-angular-route.min.js"></script>
+  <script src="./js/000-lib/005-angular-route-segment.min.js"></script>
+
+
+  <!-- Include CSS -->
+  <link rel="stylesheet" href="./css/000-jquery-ui.min.css" type="text/css"/>
+  <link rel="stylesheet" href="./css/010-bootstrap.min.css" type="text/css"/>
+  <link rel="stylesheet" href="./css/020-font-awesome.min.css" type="text/css"/>
+
+</head>
+<body>
+<div>
+  <h1>Hello</h1>
+</div>
+</body>
+</html>
